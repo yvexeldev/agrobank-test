f911b74d80ba15c0393a2241c502bbdc703bf5d4
yvexeldev <azamabdusalomov@icloud.com>
2024-10-28 11:43:14 +0500

    update location inputtype

--- src/location/dto/update-location.input.ts
+++ src/location/dto/update-location.input.ts
@@ -1,8 +1,35 @@
-import { CreateLocationInput } from './create-location.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { InputType, Field } from '@nestjs/graphql';
+import { IsOptional, IsString } from 'class-validator';
 
 @InputType()
-export class UpdateLocationInput extends PartialType(CreateLocationInput) {
-    @Field(() => Int)
-    id: number;
+export class UpdateLocationInput {
+    @Field(() => String, {
+        description: 'Title of the location',
+        nullable: true,
+    })
+    @IsOptional()
+    @IsString({ message: 'Title must be a string' })
+    title?: string;
+
+    @Field(() => String, {
+        description: 'Longitude of the location',
+        nullable: true,
+    })
+    @IsOptional()
+    @IsString({ message: 'Longitude must be a string' })
+    // @Matches(/^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)$/, {
+    //     message: 'Longitude must be a valid coordinate between -180 and 180',
+    // })
+    longitude?: string;
+
+    @Field(() => String, {
+        description: 'Latitude of the location',
+        nullable: true,
+    })
+    @IsOptional()
+    @IsString({ message: 'Latitude must be a string' })
+    // @Matches(/^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)$/, {
+    //     message: 'Latitude must be a valid coordinate between -90 and 90',
+    // })
+    latitude?: string;
 }
